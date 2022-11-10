20484d19512ae6aec7096959319961b607611e0b
Mikhail Belov <bezprobeloff@gmail.com>
2022-11-10 11:54:33 +0300

    feat: человекопонятное сообщение ошибки 500

--- middlewares/centralError.js
+++ middlewares/centralError.js
@@ -1,6 +1,6 @@
 const centralError = (err, req, res, next) => {
   if (!err.statusCode) {
-    res.status(500).send({ message: err.message });
+    res.status(500).send({ message: 'Неизвестная ошибка' });
   }
   res.status(err.statusCode).send({ message: err.message });
   next();
